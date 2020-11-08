1190392c9114bf205c3f09f08937c03199e1f827
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-08 23:00:27 +0000

    adding form to login page

--- GameTracker/app/controllers/sessions_controller.rb
+++ GameTracker/app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicaitonController
   def home
   end
-
+  
   def destroy
     session.clear
     redirect_to root_path
   end
+  #don't need to define login because there's nothing that goes in it, same with home but leaving it there for reminder
 end 
