67a1d1a2c6ebb68a44ad7227c625d9db6719e33e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-08 23:58:47 +0000

    updated game control

--- GameTracker/app/controllers/games_controller.rb
+++ GameTracker/app/controllers/games_controller.rb
@@ -1,4 +1,24 @@
 class GamesController < ApplicationController
   before_action :redirect_if_not_logged_in
   
+  def new
+    @game = Game.new
+  end
+  
+  def create
+    #create game to be rated and review
+    @game = current_user.posts.build(game_params)
+    if @game.save
+      redirect_to games_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:game).permit(:title,:content)
+  end
+  
 end
