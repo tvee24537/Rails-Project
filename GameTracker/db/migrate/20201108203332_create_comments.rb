35ba82ec930cbc03e7a5b473039787eb066fac94
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-08 20:44:53 +0000

    fixing migration table

--- GameTracker/db/migrate/20201108203332_create_comments.rb
+++ GameTracker/db/migrate/20201108203332_create_comments.rb
@@ -2,9 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :content
-      t.resources :user
-      t.resources :post
-      
+      t.references :user, foreign_key: true
+      t.references :game, foreign_key: true
 
       t.timestamps
     end
