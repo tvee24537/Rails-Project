e974f1557f513ed2a7cd760735e7201518f4468c
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-08 20:36:39 +0000

    adding comment model

---
+++ GameTracker/db/migrate/20201108203332_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.string :content
+      t.resources :user
+      t.resources :post
+      
+
+      t.timestamps
+    end
+  end
+end
