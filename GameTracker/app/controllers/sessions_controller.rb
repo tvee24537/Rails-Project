b4133f00b6bf530b41627b9272795133ca89ace4
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-08 23:25:08 +0000

    added flash and def of create in session

--- GameTracker/app/controllers/sessions_controller.rb
+++ GameTracker/app/controllers/sessions_controller.rb
@@ -7,4 +7,18 @@ class SessionsController < ApplicaitonController
     redirect_to root_path
   end
   #don't need to define login because there's nothing that goes in it, same with home but leaving it there for reminder
+  
+  def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user) #or "/users/#{user.id} for long form"
+    else 
+      flash[:message] = "Incorrect login information."
+      redirect_to "/login"
+      
+      
+    end
+  end
+  
 end 
