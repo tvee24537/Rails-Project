8edf5f965394345e38c9cbd8218ae2136057cf9a
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-12 21:37:11 +0000

    add more functions to comment control

--- GameTracker/app/controllers/comments_controller.rb
+++ GameTracker/app/controllers/comments_controller.rb
@@ -1,6 +1,13 @@
 class CommentsController < ApplicationController
   before_action :redirect_if_not_logged_in
   
+  def index
+    if params[:game_id] && @game = Game.find_by_id(params[:game_id]) #nested check
+      @comments = @game.comments
+    else  
+    @comments = Comment.all
+  end
+  
   def new
     @comment = Comment.new
   end
@@ -14,6 +21,21 @@ class CommentsController < ApplicationController
     end
   end
   
+  def show
+    @comment = Comment.find_by(id: params[:id])  
+  end  
+  
+  def edit
+    @comment = Comment.find_by(id: params[:id])  
+  end  
+  
+  def update
+    @comment = Comment.find_by(id: params[:id])  
+    if @comment.update(comment_params)
+      redirect_to comments_path(@comment)
+    end  
+  end  
+    
   private
   
   def comment_params
