0c421207b62fe0d403f1ee9f7a8ed288c2301655
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-13 22:42:11 +0000

    added edit authorization function

--- GameTracker/app/controllers/application_controller.rb
+++ GameTracker/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   #give access to methods in the views
-  helper_method :current_user, :logged_in?
+  before_action :redirect_if_not_logged_in 
+  helper_method :current_user, :logged_in?, :authorized_to_edit?
   
   private   
     def current_user
@@ -10,8 +11,12 @@ class ApplicationController < ActionController::Base
     def logged_in?
       !!session[:user_id]
     end 
+    
+    def authorized_to_edit?(game)
+      game.user == current_user
+    end  
   
     def redirect_if_not_logged_in
-      redirect_to '/' if !logged_in?
+      redirect_to '/' if !logged_in?  #or redirect_to login_path
     end
 end
