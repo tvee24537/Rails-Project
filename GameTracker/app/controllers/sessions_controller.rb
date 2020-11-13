3759e3de4f47764e973298737b35840e58ede0c3
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-13 23:59:00 +0000

    updating session control for omniauth

--- GameTracker/app/controllers/sessions_controller.rb
+++ GameTracker/app/controllers/sessions_controller.rb
@@ -9,16 +9,35 @@ class SessionsController < ApplicaitonController
     redirect_to root_path
   end
 
-  
   def create
-    user = User.find_by(username: params[:user][:username])
-    if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user) #or "/users/#{user.id} for long form"
-    else 
-      flash[:message] = "Incorrect login information."
-      redirect_to "/login"
+    if params[:provider] == 'google'
+      @user = User.create_by_google_omniauth(auth)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id 
+        redirect_to user_path(@user)
+      else
+        flash[:error] = "Your login information was incorrect. Please try again."
+        redirect_to login_path
+      end
     end
   end
+  
+  def omniauth
+    @user = User.create_by_google_omniauth(auth)
+  
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+  
+  private
+  
+  def auth
+    request.env['omniauth.auth']
+  end
+   
   
 end 
