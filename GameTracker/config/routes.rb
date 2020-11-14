0bf13d2eeec019dc1debcdd3cd66b5ae7fa952b2
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-14 01:30:03 +0000

    updating omniauth

--- GameTracker/config/routes.rb
+++ GameTracker/config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
   resources :games do
     resources :reviews, only: [:new, :index]
   end
-
+  
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
