87c4c6a39d1fc3be4ad6221e29f5f733946eef40
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-14 00:02:18 +0000

    updating user model for omniauth

--- GameTracker/app/models/user.rb
+++ GameTracker/app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true 
   validates :email, presence: true 
   
+  def self.create_by_google_omniauth(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u|
+      u.email = auth[:info][:email]  
+      u.password = SecureRandom.hex
+  
+    end
+  end
+  
 end
