c181afeaeff1beda8e828f57a9e8fc56a9568db6
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-14 00:25:58 +0000

    saving

--- GameTracker/db/schema.rb
+++ GameTracker/db/schema.rb
@@ -12,43 +12,5 @@
 
 ActiveRecord::Schema.define(version: 2020_11_08_203747) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "content"
-    t.integer "user_id"
-    t.integer "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["game_id"], name: "index_comments_on_game_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "games", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "user_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_games_on_category_id"
-    t.index ["user_id"], name: "index_games_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "comments", "games"
-  add_foreign_key "comments", "users"
-  add_foreign_key "games", "categories"
-  add_foreign_key "games", "users"
 end
