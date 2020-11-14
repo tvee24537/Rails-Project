0e475d450215ffbbc93f0f63d5301d64294c8d16
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-14 01:24:40 +0000

    updating omniauth

--- GameTracker/app/controllers/sessions_controller.rb
+++ GameTracker/app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicaitonController
   end
 
   def create
-    if params[:provider] == 'google'
+    if params[:provider] == 'google_oauth2'
       @user = User.create_by_google_omniauth(auth)
       session[:user_id] = @user.id
       redirect_to user_path(@user)
