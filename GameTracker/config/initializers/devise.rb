3c03421a577da5537ae2396d8dd67b17aaa09d52
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-14 01:19:13 +0000

    updating omniauth

--- GameTracker/config/initializers/devise.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-
-  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
-
-end
--- GameTracker/db/migrate/20201108203134_create_users.rb
+++ GameTracker/db/migrate/20201108203134_create_users.rb
@@ -4,7 +4,10 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :username
       t.string :email
       t.string :password_digest
-
+      t.string :full_name
+      t.string :uid
+      t.string :avatar_url
+      
       t.timestamps
     end
   end
