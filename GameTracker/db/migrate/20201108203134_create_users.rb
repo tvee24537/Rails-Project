9abeae3b3fee3e9850cfabb885c7ce102c6690da
tvee24537 <t.veerakajorn@gmail.com>
2020-11-15 09:52:31 -0500

    updating user table

--- GameTracker/db/migrate/20201108203134_create_users.rb
+++ GameTracker/db/migrate/20201108203134_create_users.rb
@@ -4,9 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :username
       t.string :email
       t.string :password_digest
-      t.string :full_name
-      t.string :uid
-      t.string :avatar_url
       
       t.timestamps
     end
