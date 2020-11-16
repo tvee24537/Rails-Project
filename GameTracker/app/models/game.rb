576390b76fd7539fc28aedee2d79b6148734c279
tvee24537 <t.veerakajorn@gmail.com>
2020-11-16 14:23:07 -0500

    added category index view

--- GameTracker/app/models/game.rb
+++ GameTracker/app/models/game.rb
@@ -4,7 +4,6 @@ class Game < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   
-
   validates :name, presence: true  
   validates :release_date, uniqueness: true  
   validate :not_a_duplicate 
--- GameTracker/app/models/review.rb
+++ GameTracker/app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :game
 
   validates :title, presence: true  
-  validates :title, length: {maximum: 25}
+  validates :title, length: {maximum: 35}
   validates :content, length: {maximum: 250} 
   validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
   validates :game, uniqueness: { scope: :user, message: "has already been reviewed by you"  }
