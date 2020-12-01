1de724cf6bab6814c89801cc61d625f0bd065105
tvee24537 <t.veerakajorn@gmail.com>
2020-12-01 14:22:28 -0500

    adding comments to game contrl

--- GameTracker/app/controllers/categories_controller.rb
+++ GameTracker/app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :redirect_if_not_logged_in
 
+  # category index uses scope to sort by alphabetical order
   def index 
     @categories = Category.alpha 
   end 
--- GameTracker/app/controllers/games_controller.rb
+++ GameTracker/app/controllers/games_controller.rb
@@ -2,11 +2,12 @@ class GamesController < ApplicationController
   before_action :redirect_if_not_logged_in
   before_action :set_game, only: [:show, :edit, :update] 
   
+  # make new game
   def new 
     @game = Game.new  
     @game.build_category 
   end 
-
+  # save new game with existing category or make a new category
   def create
     @game = Game.new(game_params) 
     @game.user_id = session[:user_id] 
@@ -18,7 +19,7 @@ class GamesController < ApplicationController
       render :new 
     end
   end
-
+  # index for cateory uses scope to sort games in each category by rating
   def index   
     if params[:category_id]
       category = Category.find(params[:category_id])
@@ -31,7 +32,7 @@ class GamesController < ApplicationController
   def show 
     
   end 
-
+  # edit game function
   def edit 
     if authorized_to_edit?(@game) 
       render :edit   
@@ -39,7 +40,7 @@ class GamesController < ApplicationController
       redirect_to game_path(@game)   
     end
   end 
-
+  # update the change in the edit
   def update   
     if @game.update(game_params)
       redirect_to game_path(@game)
@@ -47,22 +48,22 @@ class GamesController < ApplicationController
       render :edit
     end 
   end 
-
+  # rate game
   def most_popular 
     @games = Game.most_popular 
   end 
 
   private 
-
+  # parameter for game
   def game_params
     params.require(:game).permit(:name, :price_range, :release_date, :category_id, category_attributes: [:name])
   end
-
+  # find game
   def set_game
     @game = Game.find_by(id: params[:id])
     redirect_to games_path if !@game 
   end
-
+  # redirect if not own by current user
   def redirect_if_not_authorized 
     if @game.update(name: params[:name], price_range: params[:price_range], release_date: params[:release_date])   
       redirect_to game_path(@game)
