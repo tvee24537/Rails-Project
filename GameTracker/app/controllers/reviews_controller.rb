0d0f0a5ff3e885ead2de4778c55415069d0dac02
tvee24537 <t.veerakajorn@gmail.com>
2020-12-03 11:31:32 -0500

    adding comments to review contrl

--- GameTracker/app/controllers/reviews_controller.rb
+++ GameTracker/app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
-  
+  # check if game exist by id and make review
   def new
     if @game = Game.find_by_id(params[:game_id])
       @review = @game.reviews.build
@@ -8,7 +8,7 @@ class ReviewsController < ApplicationController
       @review = Review.new
     end
   end
-
+  # check if review saves and redirect to view page or redirect to review new page again if it doesn't save
   def create
     @review = current_user.reviews.build(review_params)
     if @review.save
@@ -17,11 +17,11 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
-
+  # function to show review by id
   def show
     @review = Review.find_by_id(params[:id])
   end
-    
+   # find review by id or list all reviews 
   def index
     if @game = Game.find_by_id(params[:game_id])
       @reviews = @game.reviews
@@ -31,7 +31,7 @@ class ReviewsController < ApplicationController
   end
 
   private 
-
+  # parameters of reviews
   def review_params
     params.require(:review).permit(:game_id, :content, :stars, :title)  
   end 
