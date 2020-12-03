d327c7efd09e9204c327218d7dd75e4cd46f2b0b
tvee24537 <t.veerakajorn@gmail.com>
2020-12-03 11:37:13 -0500

    adding comments to user contrl

--- GameTracker/app/controllers/users_controller.rb
+++ GameTracker/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
-  
+  # make new user
   def new
     @user = User.new
   end
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     #log in and redirect to the show page
-    if @user.valid?
+    if @user.valid? # validates user and save
       @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+  # check if user is logged in, if not redirect to home page
   def show
     redirect_if_not_logged_in
     @user = User.find_by_id(params[:id])
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   end  
   
   private
-    
+  # parameter for user
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
